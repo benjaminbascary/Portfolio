6d64e87045236608152372f3a9b088c6676bf1cd
benjaminbascary <benjaminbascary@gmail.com>
2022-04-16 12:15:34 -0300

    Commits final changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <div className='footer'>
-        <a href='https://www.linkedin.com/in/benjamin-bascary/' target="_blank" rel='noreferrer'>LinkedIn</a>
-        <a href='https://www.github.com/benjaminbascary/' target="_blank" rel='noreferrer'>GITHUB</a>
-        <p>Cellphone: +54 9 1159495066</p>
+        <a className="footer--link"href='https://www.linkedin.com/in/benjamin-bascary/' target="_blank" rel='noreferrer'>LinkedIn</a>
+        <a className="footer--link" href='https://www.github.com/benjaminbascary/' target="_blank" rel='noreferrer'>GITHUB</a>
+        <p className='footer--text'>Cellphone: +54 9 1159495066</p>
     </div>
   );
 };
